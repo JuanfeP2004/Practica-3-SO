ee3a53f35a6cd5f374a51c84f9f7bf6fab870809
JuanfeP2004 <juannfe2004@gmail.com>
2024-02-11 00:17:19 -0500

    Feat: se le agrego poner numeros

--- Practica3SO.c
+++ Practica3SO.c
@@ -4,36 +4,26 @@ int main() {
 
     float pila[4] = {0, 0, 0, 0};
     float numero = 0;
+    int size = 4;
 
     do {
 
-        printf("Hola, ponga un numero");
-        printf("4. %f", pila[3]);
-        printf("3. %f", pila[2]);
-        printf("2. %f", pila[1]);
-        printf("1. %f", pila[0]);
+        printf("Hola, ponga un numero\n");
+        printf("4. %f\n", pila[3]);
+        printf("3. %f\n", pila[2]);
+        printf("2. %f\n", pila[1]);
+        printf("1. %f\n", pila[0]);
 
         scanf("%f", &numero);
+        
+        for(int i = 0; i < size; i = i+1){
+            if(pila[i] == 0) {
+                pila[i] = numero;
+                break;
+            }
+        }
     }
     while (0 == 0);
 
     return 0;
 }
-
-/*void hola(int numero){
-    
-    if (pila[0] == 0) {
-        
-    }
-    else if (pila[2] == 0){
-        
-    }
-    else if (pila[2] == 0){
-        
-    }
-    else if (pila[3] == 0){
-        
-    }
-
-    return;
-}*/
